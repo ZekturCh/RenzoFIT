00476da330dd033bcedda81a00a8bdd76f5d1b7e
ZekturCh <jchavezch01@outlook.com>
2026-05-14 01:48:50 -0500

    Create constants.js

---
+++ assets/js/constants.js
@@ -0,0 +1,26 @@
+// assets/js/constants.js
+
+export const ROLES = {
+  ADMIN: "admin",
+  BASIC: "basic"
+};
+
+export const ORDER_STATUS = {
+  COTIZANDO: "cotizando",
+  DIAGNOSTICANDO: "diagnosticando",
+  LISTO_TRABAJAR: "listo_trabajar",
+  TRABAJO_TERMINADO: "trabajo_terminado",
+  POR_COBRAR: "por_cobrar",
+  POR_RECOGER: "por_recoger",
+  ENTREGADO: "entregado",
+  NO_VA: "no_va"
+};
+
+export const PAYMENT_METHODS = {
+  EFECTIVO: "efectivo",
+  YAPE: "yape",
+  PLIN: "plin",
+  TRANSFERENCIA: "transferencia",
+  TARJETA: "tarjeta",
+  OTRO: "otro"
+};
