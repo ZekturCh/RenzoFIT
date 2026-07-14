b105afdf7fcc03bdf4ef516e24e1d0cdbac54715
ZekturCh <jchavezch01@outlook.com>
2026-07-14 00:00:58 -0500

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -44,6 +44,7 @@ export function toDateInputValue(date) {
   const year = d.getFullYear();
   const month = String(d.getMonth() + 1).padStart(2, "0");
   const day = String(d.getDate()).padStart(2, "0");
+
   return `${year}-${month}-${day}`;
 }
 
